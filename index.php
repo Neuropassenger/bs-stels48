3825474ddcfe6073c7785339689512245084519a
Oleg Sokolov <turgenoid@gmail.com>
2019-09-08 09:49:42 +0300

    css

--- index.php
+++ index.php
@@ -8,7 +8,9 @@
     <meta name="description" content="STELS 48">
     <meta name="author" content="Breakout Studio">
 
+    <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/main.css?v=1.0.0">
+    <link rel="stylesheet" href="css/media.css?v=1.0.0">
 
 </head>
 
