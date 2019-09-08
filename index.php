f477e05361ff3856ea86ebfb666c3fe76ec87223
gumarov1991 <gumarov2017@yandex.ru>
2019-09-08 21:27:24 +0600

    catalog first commit

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/main.css?v=1.0.0">
     <link rel="stylesheet" href="css/media.css?v=1.0.0">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -24,8 +25,224 @@
     Первый экран
 </section>
 
-<section>
-    Каталог
+<section class="a_catalog">
+    <div>
+        <h2>Каталог</h2>
+    </div>
+    <div class="a_goods flex_main flex_wrap flex_jcontent-around">
+        <div class="a_position">
+            <h4>STELS Navigator 630 MD</h4>
+            <img src="img/navigator630.png" alt="stells navigator 630">
+            <table>
+                <tr>
+                    <td>Тип рамы</td>
+                    <td>алюминий</td>
+                </tr>
+                <tr>
+                    <td>Размер рамы</td>
+                    <td>20</td>
+                </tr>
+                <tr>
+                    <td>Размер колес</td>
+                    <td>26</td>
+                </tr>
+                <tr>
+                    <td>Количество скоростей</td>
+                    <td>21</td>
+                </tr>
+                <tr>
+                    <td>Тип тормозов</td>
+                    <td>дисковые</td>
+                </tr>
+                <tr>
+                    <td>Цвет</td>
+                    <td>серый/синий</td>
+                </tr>
+                <tr>
+                    <td>Цена</td>
+                    <td>19 000</td>
+                </tr>
+            </table>
+            <button class="a_catalog_button">заказать</button>
+        </div>
+        <div class="a_position">
+            <h4>STELS Navigator 300 Gent</h4>
+            <img src="img/navigator300gent.png" alt="stells navigator 300">
+            <table>
+                <tr>
+                    <td>Тип рамы</td>
+                    <td>сталь</td>
+                </tr>
+                <tr>
+                    <td>Размер рамы</td>
+                    <td>20</td>
+                </tr>
+                <tr>
+                    <td>Размер колес</td>
+                    <td>28</td>
+                </tr>
+                <tr>
+                    <td>Количество скоростей</td>
+                    <td>1</td>
+                </tr>
+                <tr>
+                    <td>Тип тормозов</td>
+                    <td>задний ножной</td>
+                </tr>
+                <tr>
+                    <td>Цвет</td>
+                    <td>синий/серый</td>
+                </tr>
+                <tr>
+                    <td>Цена</td>
+                    <td>6 500</td>
+                </tr>
+            </table>
+            <button class="a_catalog_button">заказать</button>
+        </div>
+        <div class="a_position">
+            <h4>STELS Miss 5000 MD</h4>
+            <img src="img/miss5000md.png" alt="stells miss 5000">
+            <table>
+                <tr>
+                    <td>Тип рамы</td>
+                    <td>сталь</td>
+                </tr>
+                <tr>
+                    <td>Размер рамы</td>
+                    <td>17</td>
+                </tr>
+                <tr>
+                    <td>Размер колес</td>
+                    <td>26</td>
+                </tr>
+                <tr>
+                    <td>Количество скоростей</td>
+                    <td>21</td>
+                </tr>
+                <tr>
+                    <td>Тип тормозов</td>
+                    <td>дисковые</td>
+                </tr>
+                <tr>
+                    <td>Цвет</td>
+                    <td>бирюзовый</td>
+                </tr>
+                <tr>
+                    <td>Цена</td>
+                    <td>13 720</td>
+                </tr>
+            </table>
+            <button class="a_catalog_button">заказать</button>
+        </div>
+        <div class="a_position">
+            <h4>STELS Focus MD</h4>
+            <img src="img/focusmd.png" alt="stells focus MD">
+            <table>
+                <tr>
+                    <td>Тип рамы</td>
+                    <td>сталь</td>
+                </tr>
+                <tr>
+                    <td>Размер рамы</td>
+                    <td>18</td>
+                </tr>
+                <tr>
+                    <td>Размер колес</td>
+                    <td>26</td>
+                </tr>
+                <tr>
+                    <td>Количество скоростей</td>
+                    <td>21</td>
+                </tr>
+                <tr>
+                    <td>Тип тормозов</td>
+                    <td>дисковые</td>
+                </tr>
+                <tr>
+                    <td>Цвет</td>
+                    <td>серый/синий</td>
+                </tr>
+                <tr>
+                    <td>Цена</td>
+                    <td>14 600</td>
+                </tr>
+            </table>
+            <button class="a_catalog_button">заказать</button>
+        </div>
+        <div class="a_position">
+            <h4>STELS Pilot 950 MD</h4>
+            <img src="img/pilot950md.png" alt="stells pilot 950">
+            <table>
+                <tr>
+                    <td>Тип рамы</td>
+                    <td>алюминий</td>
+                </tr>
+                <tr>
+                    <td>Размер рамы</td>
+                    <td>17.5</td>
+                </tr>
+                <tr>
+                    <td>Размер колес</td>
+                    <td>26</td>
+                </tr>
+                <tr>
+                    <td>Количество скоростей</td>
+                    <td>21</td>
+                </tr>
+                <tr>
+                    <td>Тип тормозов</td>
+                    <td>дисковые</td>
+                </tr>
+                <tr>
+                    <td>Цвет</td>
+                    <td>темно-синий</td>
+                </tr>
+                <tr>
+                    <td>Цена</td>
+                    <td>17 350</td>
+                </tr>
+            </table>
+            <button class="a_catalog_button">заказать</button>
+        </div>
+        <div class="a_position">
+            <h4>STELS Navigator 500 V</h4>
+            <img src="img/navigator500v.png" alt="stells navigator 500">
+            <table>
+                <tr>
+                    <td>Тип рамы</td>
+                    <td>сталь</td>
+                </tr>
+                <tr>
+                    <td>Размер рамы</td>
+                    <td>20</td>
+                </tr>
+                <tr>
+                    <td>Размер колес</td>
+                    <td>26</td>
+                </tr>
+                <tr>
+                    <td>Количество скоростей</td>
+                    <td>21</td>
+                </tr>
+                <tr>
+                    <td>Тип тормозов</td>
+                    <td>ободные</td>
+                </tr>
+                <tr>
+                    <td>Цвет</td>
+                    <td>серый/синий</td>
+                </tr>
+                <tr>
+                    <td>Цена</td>
+                    <td>12 300</td>
+                </tr>
+            </table>
+            <button class="a_catalog_button">заказать</button>
+        </div>
+    </div>
+
+
 </section>
 
 <section>
