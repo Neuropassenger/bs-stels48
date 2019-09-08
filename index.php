27e155d9039953bac255c9790bfcb8f6392d7aa2
Oleg Sokolov <turgenoid@gmail.com>
2019-09-08 09:46:52 +0300

    base structure

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+
+    <title>STELS 48</title>
+    <meta name="description" content="STELS 48">
+    <meta name="author" content="Breakout Studio">
+
+    <link rel="stylesheet" href="css/main.css?v=1.0.0">
+
+</head>
+
+<body>
+<script src="js/common.js"></script>
+
+<header>
+    Шапка сайта
+</header>
+
+<section>
+    Первый экран
+</section>
+
+<section>
+    Каталог
+</section>
+
+<section>
+    О нас
+</section>
+
+<section>
+    Оплата и доставка
+</section>
+
+<section>
+    О велосипедах STELS
+</section>
+
+<section>
+    Отзывы
+</section>
+
+<section>
+    Контакты
+</section>
+
+<footer>
+    Подвал
+</footer>
+
+</body>
+</html>
