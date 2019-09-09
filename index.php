fef217a9d7f43b49cd168e2620ce53d423fdf258
gumarov1991 <gumarov2017@yandex.ru>
2019-09-09 18:48:17 +0600

    about bikes delivery payment

--- index.php
+++ index.php
@@ -12,9 +12,11 @@
     <link rel="stylesheet" href="css/main.css?v=1.0.0">
     <link rel="stylesheet" href="css/media.css?v=1.0.0">
     <link rel="stylesheet" href="libs/flex_css/flex.css">
-    <link rel="stylesheet" href="libs/lightbox/lightbox.min.css">
+    <link rel="stylesheet" href="libs/fancybox/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
-    <script src="libs/lightbox/lightbox-plus-jquery.min.js"></script>
+    <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="libs/fancybox/jquery.fancybox.min.js"></script>
 </head>
 
 <body>
@@ -35,7 +37,7 @@
     <div class="a_goods flex_main flex_wrap flex_jcontent-around">
         <div class="a_position">
             <h4>STELS Navigator 630 MD</h4>
-            <a href="img/navigator630@2x.png" data-lightbox="bikes"><img src="img/navigator630.png" alt="stells navigator 630"></a>
+            <img src="img/navigator630.png" alt="stells navigator 630">
             <table>
                 <tr>
                     <td>Тип рамы</td>
@@ -66,11 +68,15 @@
                     <td>19 000</td>
                 </tr>
             </table>
-            <button class="a_catalog_button">заказать</button>
+            <button class="a_catalog_button" data-fancybox data-src="#hidden-navigator-630" href="javascript:;" >заказать</button>
+            <div style="display: none;" id="hidden-navigator-630">
+	            <h2>Здесь!</h2>
+	            <p>Будет форма)<br>пока сдеал для каждой модели отдельный скрытый div с индивидуальным id</p>
+            </div>
         </div>
         <div class="a_position">
             <h4>STELS Navigator 300 Gent</h4>
-            <a href="img/navigator300gent@2x.png" data-lightbox="bikes"><img src="img/navigator300gent.png" alt="stells navigator 300"></a>
+            <img src="img/navigator300gent.png" alt="stells navigator 300">
             <table>
                 <tr>
                     <td>Тип рамы</td>
@@ -101,11 +107,15 @@
                     <td>6 500</td>
                 </tr>
             </table>
-            <button class="a_catalog_button">заказать</button>
+            <button class="a_catalog_button" data-fancybox data-src="#hidden-navigator-300" href="javascript:;" >заказать</button>
+            <div style="display: none;" id="hidden-navigator-300">
+	            <h2>Здесь!</h2>
+	            <p>Будет форма)<br>пока сдеал для каждой модели отдельный скрытый div с индивидуальным id</p>
+            </div>
         </div>
         <div class="a_position">
             <h4>STELS Miss 5000 MD</h4>
-            <a href="img/miss5000md@2x.png" data-lightbox="bikes"><img src="img/miss5000md.png" alt="stells miss 5000"></a>
+            <img src="img/miss5000md.png" alt="stells miss 5000">
             <table>
                 <tr>
                     <td>Тип рамы</td>
@@ -136,11 +146,15 @@
                     <td>13 720</td>
                 </tr>
             </table>
-            <button class="a_catalog_button">заказать</button>
+            <button class="a_catalog_button" data-fancybox data-src="#hidden-miss-5000" href="javascript:;" >заказать</button>
+            <div style="display: none;" id="hidden-miss-5000">
+	            <h2>Здесь!</h2>
+	            <p>Будет форма)<br>пока сдеал для каждой модели отдельный скрытый div с индивидуальным id</p>
+            </div>
         </div>
         <div class="a_position">
             <h4>STELS Focus MD</h4>
-            <a href="img/focusmd@2x.png" data-lightbox="bikes"><img src="img/focusmd.png" alt="stells focus MD"></a>
+            <img src="img/focusmd.png" alt="stells focus MD">
             <table>
                 <tr>
                     <td>Тип рамы</td>
@@ -171,11 +185,15 @@
                     <td>14 600</td>
                 </tr>
             </table>
-            <button class="a_catalog_button">заказать</button>
+            <button class="a_catalog_button" data-fancybox data-src="#hidden-focus" href="javascript:;" >заказать</button>
+            <div style="display: none;" id="hidden-focus">
+	            <h2>Здесь!</h2>
+	            <p>Будет форма)<br>пока сдеал для каждой модели отдельный скрытый div с индивидуальным id</p>
+            </div>
         </div>
         <div class="a_position">
             <h4>STELS Pilot 950 MD</h4>
-            <a href="img/pilot950md@2x.png" data-lightbox="bikes"><img src="img/pilot950md.png" alt="stells pilot 950"></a>
+            <img src="img/pilot950md.png" alt="stells pilot 950">
             <table>
                 <tr>
                     <td>Тип рамы</td>
@@ -206,11 +224,15 @@
                     <td>17 350</td>
                 </tr>
             </table>
-            <button class="a_catalog_button">заказать</button>
+            <button class="a_catalog_button" data-fancybox data-src="#hidden-pilot-950" href="javascript:;" >заказать</button>
+            <div style="display: none;" id="hidden-pilot-950">
+	            <h2>Здесь!</h2>
+	            <p>Будет форма)<br>пока сдеал для каждой модели отдельный скрытый div с индивидуальным id</p>
+            </div>
         </div>
         <div class="a_position">
             <h4>STELS Navigator 500 V</h4>
-            <a href="img/navigator500v@2x.png" data-lightbox="bikes"><img src="img/navigator500v.png" alt="stells navigator 500"></a>
+            <img src="img/navigator500v.png" alt="stells navigator 500">
             <table>
                 <tr>
                     <td>Тип рамы</td>
@@ -241,23 +263,103 @@
                     <td>12 300</td>
                 </tr>
             </table>
-            <button class="a_catalog_button">заказать</button>
+            <button class="a_catalog_button" data-fancybox data-src="#hidden-navigator-500" href="javascript:;" >заказать</button>
+            <div style="display: none;" id="hidden-navigator-500">
+	            <h2>Здесь!</h2>
+	            <p>Будет форма)<br>пока сдеал для каждой модели отдельный скрытый div с индивидуальным id</p>
+            </div>
         </div>
     </div>
-
-
 </section>
 
-<section>
-    О нас
+<section class="a_about">
+    <div>
+        <h2>О нас</h2>
+    </div>
+    <div class="a_wrap flex_main flex_wrap flex_jcontent-around">
+        <img src="img/quality.jpg" alt="качественные велосипеды">
+        <div class="a_decrip flex_main flex_column flex_jcontent-around">
+            <h4>Качественный товар</h4>
+            <p>Наша команда увлечена прогулками на велосипедах, и у нас самих есть модели, представленные на сайте.</p>
+            <p>После продолжительного использования в разных условиях были приятно удивлены качеством велосипедов и на своем опыте убедились, что STELS выпускает надежные велосипеды, достойные наших рекомендаций.</p>
+        </div>
+    </div>
+    <div class="a_wrap flex_main flex_wrap flex_row-revers flex_jcontent-around">
+        <img src="img/stels.png" alt="качественные велосипеды">
+        <div class="a_decrip flex_main flex_column flex_jcontent-around">
+            <h4>Выгодные цены</h4>
+            <p>Наши основные направления - это продажа запчастей для иномарок и мотоциклов. У нас нет отдельного склада для велосипедов и мы не держим их в наличии, от чего существенно снижаются издержки.</p>
+            <p>Мы привозим велосипед в коробке с завода только после вашего заказа в течение 24 часов. Как результат - у нас одни из самых выгодных цен на рынке.</p>
+        </div>
+    </div>
+    <div class="a_wrap flex_main flex_wrap flex_jcontent-around">
+        <img src="img/davai.png" alt="качественные велосипеды">
+        <div class="a_decrip flex_main flex_column flex_jcontent-around">
+            <h4>Активный образ жизни</h4>
+            <p>Будучи истинными любителями велоспорта, мы стремимся приобщить как можно больше людей к активному образу жизни. Велосипед - это самый простой способ начать больше двигаться.</p>
+            <p>Поэтому предлагаем цены ниже, чем у конкурентов. Для нас важнее сделать как можно больше людей здоровее, чем заработать с продажи лишнюю тысячу рублей.</p>
+        </div>
+    </div>
 </section>
 
-<section>
-    Оплата и доставка
+<section class="a_conditions">
+    <div  class="flex_main flex_wrap flex_jcontent-around">
+        <div class="a_payment">
+            <h2>Оплата</h2>
+            <ul class="fa-ul">
+                <li><i class="fa-li fa fa-ruble"></i>Оплатить покупку вы можете наличными в нашем розничном магазине, либо переводом на карту Сбербанка.</li>
+                <li><i class="fa-li fa fa-ruble"></i>При покупке велосипеда в розничном магазине необходимо оплатить половину стоимости в момент оформления заказа. И остальную половину при непосредственном получении велосипеда.</li>
+                <li><i class="fa-li fa fa-ruble"></i>При покупке велосипеда через интернет необходимо внести всю стоимость оплаты в момент оформления заказа.</li>
+            </ul>
+        </div>
+        <div class="a_delivery">
+            <h2>Доставка</h2>
+            <ul class="fa-ul">
+                <li><i class="fa-li fa fa-truck"></i>Доставка велосипедов по городу Ельцу производится только в несобранном состоянии. Мы привозим вам запечатанную коробку с велосипедом с завода.</li>
+                <li><i class="fa-li fa fa-truck"></i>Если требуется сборка, то велосипед придется забрать самим из нашего розничного магазина.</li>
+                <li><i class="fa-li fa fa-truck"></i>Доставка по России осуществляется любым удобным для вас способом (Почта РФ, транспортные компании). Это согласуется во время телефонного звонка с нашим менеджером.</li>
+            </ul>
+        </div>
+    </div>
+    <div class="center">
+        <button data-fancybox data-src="#hidden-general-order" href="javascript:;">заказать</button>
+    </div>
 </section>
 
-<section>
-    О велосипедах STELS
+<section class="a_about_bikes">
+<div>
+    <h2>О велосипедах STELS</h2>
+    </div>
+    <div class="a_wrap flex_main flex_wrap flex_jcontent-around">
+        <img src="img/zavod1.png" alt="завод велосипедов">
+        <div class="a_decrip flex_main flex_column flex_jcontent-around">
+            <h4>Велосипеды STELS производятся в России</h4>
+            <p>Компания STELS является обладателем двух заводов на территории России. Это Жуковский и Кубинский велозаводы.</p>
+            <p>Эти два завода охватывают полный цикл производства велосипедов. Начиная от создания рам и вилок и заканчивая окраской готовых изделий. Большая часть производственных мощностей автоматизирована и имеет в своем составе роботизированные станки из Японии, Голландии и Германии.</p>
+        </div>
+    </div>
+    <div class="a_wrap flex_main flex_wrap flex_row-revers flex_jcontent-around">
+        <img src="img/control.png" alt="гарантия качества велоспедов">
+        <div class="a_decrip flex_main flex_column flex_jcontent-around">
+            <h4>Гарантия качества</h4>
+            <p>В процессе производства имеет место обязательный трехэтапный контроль качества:</p>
+            <ul>
+                <li>Проверка входных комплектующий на отсутствие повреждений и соответствие спецификации</li>
+                <li>Контроль за корректной установкой комплектующих на раму, настройка и проверка навесного оборудования</li>
+                <li>Проверка комплектности и правильности укладки велосипеда в коробку</li>
+            </ul> 
+        </div>
+    </div>
+    <div class="a_wrap flex_main flex_wrap flex_jcontent-around">
+        <img src="img/manufact_2.png" alt="современные технологии производства велосипедов">
+        <div class="a_decrip flex_main flex_column flex_jcontent-around">
+            <h4>Современные технологии</h4>
+            <p>Велосипеды STELS создаются с применением современных технологий: <b>картриджные каретки</b> (устойчивость к попаданию грязи, не требуется обслуживание), <b>дисковые тормоза</b> (надежное торможение даже при повреждении обода, меньшая чувствительность к дождю и грязи), <b>гидроформинг</b> (придание раме необходимой формы без потери прочности), <b>конический рулевой стакан</b> (меньшая нагрузка на шток вилки и подшипники рулевой колонки, устойчивость и стабильность в поворотах), <b>блокировка вилки</b> (гибкость в выборе вида трассы), <b>планетарная втулка</b> (надежное переключение передач, защита от внешней среды).</p>
+        </div>
+    </div>
+    <div class="center">
+        <button data-fancybox data-src="#hidden-general-order" href="javascript:;">заказать</button>
+    </div>
 </section>
 
 <section>
@@ -271,6 +373,9 @@
 <footer>
     Подвал
 </footer>
-
+<div style="display: none;" id="hidden-general-order">
+    <h2>Здесь!</h2>
+    <p>Будет форма для заказа из общих кнопок "заказать"</p>
+</div>
 </body>
 </html>
