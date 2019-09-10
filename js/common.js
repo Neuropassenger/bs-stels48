0f8fd65944610ac105b791e6899d0cb158ea5e33
gumarov1991 <gumarov2017@yandex.ru>
2019-09-10 20:15:53 +0600

    fix little problems

--- js/common.js
+++ js/common.js
@@ -5,3 +5,28 @@ $(document).ready(function(){
         nav: true
     });
 });
+
+ymaps.ready(init);
+    function init(){  
+        var myMap = new ymaps.Map("map", {
+            center: [52.62166957, 38.50424550],
+            zoom: 16
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myGeoObject = new ymaps.GeoObject({
+            geometry: {
+                type: "Point",
+                coordinates: [52.621670, 38.504245]
+            },
+            properties: {
+                iconContent: 'STELLS',
+                hintContent: 'Качественные велосипеды по самым низким ценам в СНГ'
+            }
+        }, {
+            preset: 'islands#blackStretchyIcon',
+            draggable: true
+        });
+        myMap.geoObjects
+            .add(myGeoObject);
+    }
