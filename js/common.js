2916f2ee6d67881c26e8fd53e333ddd1558f2039
gumarov1991 <gumarov2017@yandex.ru>
2019-09-11 15:41:12 +0600

    fix map arrows lineheight

--- js/common.js
+++ js/common.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
     $("#a_comments").owlCarousel({
         items: 1,
         loop: true,
-        nav: true
+        nav: true,
+        navText: ["<img src='img/arrow_left.png'>", "<img src='img/arrow_right.png'>"]
     });
 
     $(".a_footer_menu").on("click","a", function (event) {
@@ -21,33 +22,28 @@ $(document).ready(function(){
 
 });
 
-$(document).ready(function(){
-    
-});
-
 ymaps.ready(init);
-function init(){  
+
+function init() {  
     var myMap = new ymaps.Map("map", {
-        center: [52.62166957, 38.50424550],
-        zoom: 16
+        center: [52.621670, 38.504245],
+        zoom: 16,
+        controls: []
     }, {
         searchControlProvider: 'yandex#search'
     }),
-    myGeoObject = new ymaps.GeoObject({
-        geometry: {
-            type: "Point",
-            coordinates: [52.621670, 38.504245]
-        },
-        properties: {
-            iconContent: 'STELLS',
-            hintContent: 'Качественные велосипеды по самым низким ценам в СНГ'
-        }
-    }, {
-        preset: 'islands#blackStretchyIcon',
-        draggable: true
-    });
-    myMap.geoObjects
-        .add(myGeoObject);
-}
-
     
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        hintContent: 'Качественные велосипеды по самым низким ценам в СНГ',
+        balloonContent: 'Россия, Липецкая обл., г. Елец, ул. Пушкина, д. 144'
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/adress.png',
+        iconImageSize: [80, 20],
+        iconImageOffset: [-5, -20]
+    });
+
+    myMap.geoObjects
+        .add(myPlacemark)
+        .add(myPlacemarkWithContent);
+};
