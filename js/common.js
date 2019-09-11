e647eab6cd11dadba385fc581d0ef3cd5b1a9f69
gumarov1991 <gumarov2017@yandex.ru>
2019-09-11 08:51:00 +0600

    change button to href

--- js/common.js
+++ js/common.js
@@ -4,29 +4,50 @@ $(document).ready(function(){
         loop: true,
         nav: true
     });
+
+    $(".a_footer_menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+    $(".a_order_wrap").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+});
+
+$(document).ready(function(){
+    
 });
 
 ymaps.ready(init);
-    function init(){  
-        var myMap = new ymaps.Map("map", {
-            center: [52.62166957, 38.50424550],
-            zoom: 16
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
-        myGeoObject = new ymaps.GeoObject({
-            geometry: {
-                type: "Point",
-                coordinates: [52.621670, 38.504245]
-            },
-            properties: {
-                iconContent: 'STELLS',
-                hintContent: 'Качественные велосипеды по самым низким ценам в СНГ'
-            }
-        }, {
-            preset: 'islands#blackStretchyIcon',
-            draggable: true
-        });
-        myMap.geoObjects
-            .add(myGeoObject);
-    }
+function init(){  
+    var myMap = new ymaps.Map("map", {
+        center: [52.62166957, 38.50424550],
+        zoom: 16
+    }, {
+        searchControlProvider: 'yandex#search'
+    }),
+    myGeoObject = new ymaps.GeoObject({
+        geometry: {
+            type: "Point",
+            coordinates: [52.621670, 38.504245]
+        },
+        properties: {
+            iconContent: 'STELLS',
+            hintContent: 'Качественные велосипеды по самым низким ценам в СНГ'
+        }
+    }, {
+        preset: 'islands#blackStretchyIcon',
+        draggable: true
+    });
+    myMap.geoObjects
+        .add(myGeoObject);
+}
+
+    
